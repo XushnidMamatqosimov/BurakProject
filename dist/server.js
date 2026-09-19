6afaacdabf45cbacbf10f997b70ab9f1909b6666
Xushnid <xmamtqosimov@gmail.com>
2026-09-19 17:28:51 +0900

    Initial setUp

---
+++ dist/server.js
@@ -0,0 +1,5 @@
+"use strict";
+console.log("Hello World");
+console.log('alo');
+const person = "Declan";
+const count = 1234;
---
+++ src/server.ts
@@ -0,0 +1,10 @@
+import moment from "moment";
+
+console.log("Hello World")
+console.log('alo')
+
+const time = moment().format("DD.mm.YYYY");
+console.log(time)
+
+const person: string = "Declan";
+const count: number = 1234;
