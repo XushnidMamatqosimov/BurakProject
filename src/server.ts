50813d17ebe2d628c749739f89e450680a7c6984
Xushnid <xmamtqosimov@gmail.com>
2026-09-19 18:38:06 +0900

    feat: integration of environmental variables

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,2 @@
-import moment from "moment";
-
-console.log("Hello World")
-console.log('alo')
-
-const time = moment().format("DD.mm.YYYY");
-console.log(time)
-
-const person: string = "Declan";
-const count: number = 1234;
+import dotenv from "dotenv";
+dotenv.config();
