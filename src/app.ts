174493927955a2c84ee22608da2ddc49f2ad8851
Xushnid <xmamtqosimov@gmail.com>
2026-09-19 19:23:59 +0900

    feat: express integration

---
+++ src/app.ts
@@ -0,0 +1,16 @@
+import express, { urlencoded } from "express";
+import { url } from "inspector";
+import path from "path";
+
+//  1-Entrence
+const app = express();
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+//  2-Sessions
+//  3-Views
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+//  4-Route
+
+export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,16 @@
 import dotenv from "dotenv";
 dotenv.config();
-
 import mongoose from "mongoose";
+import app from "./app";
+
 mongoose
     .connect(process.env.MONGO_URL as string, {})
     .then((data)=> {
         console.log("Bluetooth device is connected a Successfully");
         const PORT = process.env.PORT ?? 3003; 
+        app.listen(PORT, function () {
+            console.log(`The server is running successfully on ${PORT}`)
+        })
     })
     .catch((err)=>{
         console.log("Error on connection MongoDb", err)
