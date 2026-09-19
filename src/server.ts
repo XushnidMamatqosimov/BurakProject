25b2a982b51c5e6427aeb473dfc18a424480281f
Xushnid <xmamtqosimov@gmail.com>
2026-09-19 19:06:11 +0900

    feat: connect Mongoose

--- src/server.ts
+++ src/server.ts
@@ -1,2 +1,13 @@
 import dotenv from "dotenv";
 dotenv.config();
+
+import mongoose from "mongoose";
+mongoose
+    .connect(process.env.MONGO_URL as string, {})
+    .then((data)=> {
+        console.log("Bluetooth device is connected a Successfully");
+        const PORT = process.env.PORT ?? 3003; 
+    })
+    .catch((err)=>{
+        console.log("Error on connection MongoDb", err)
+    });
