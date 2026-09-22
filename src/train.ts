f7354a0f68372ce138875aceb0cd59ede52983bd
Xushnid <xmamtqosimov@gmail.com>
2026-09-22 20:23:25 +0900

    feat: M-Task done

--- src/train.ts
+++ src/train.ts
@@ -1,2 +1,21 @@
 console.log("Hello world");
 
+// H - Task
+const arr12 = [1,2,3,4];
+function getSquare(arr: number[]){
+    let a = 0;
+    const squaredArr = []
+
+    /* for (let i = 0; i <= arr.length; i++){
+
+        squaredArr.push(Math.pow(i,2));
+    } */
+    for(let num of arr){
+        squaredArr.push(Math.pow(num,2));
+    }
+    for(let res of squaredArr){
+        console.log(res);
+    }
+}
+
+getSquare(arr12);
